f2cbd3b279285687958a24707daa2af9d2bbc9dc
Elena_Tibekina <elentibekina@gmail.com>
2020-06-01 12:50:44 +0300

    fixed formating

--- controllers/pokemonsController.js
+++ controllers/pokemonsController.js
@@ -1,112 +1,110 @@
-const fs = require('fs');
-const Pokemon = require('../schemas/pokemon')
+const Pokemon = require('../schemas/pokemon');
 
-function writePokemons (writeCallback) {
-    fs.writeFile(`${__dirname}/../pokemons.json`, JSON.stringify(pokemons), writeCallback)
+function response(res, code, status, data) {
+  res.status(code).json({
+    status: status,
+    ...data,
+  });
 }
 
-function response (res, code, status, data) {
-    res.status(code).json({
-        status: status,
-        ...data
-    })
+function responseFail(res, errorMessage) {
+  response(res, 500, 'bad request', { message: errorMessage });
 }
 
-function responseFail (res, errorMessage) {
-    response(res, 500, 'bad request', { message: errorMessage })
-}
-
-function responseBad (res, errorMessage) {
-    response(res, 404, 'failed', { message: errorMessage })
+function responseBad(res, errorMessage) {
+  response(res, 404, 'failed', { message: errorMessage });
 }
 
 function responsePokemon(res, pokemon) {
-    response(res, 200, 'success', { data: { pokemon: pokemon} })
+  response(res, 200, 'success', { data: { pokemon: pokemon } });
 }
 
-function responsePokemons (res, pokemons) {
-    response(res, 200, 'success', {
-        results: pokemons.length,
-        data: { pokemons: pokemons }
-    })
+function responsePokemons(res, pokemons) {
+  response(res, 200, 'success', {
+    results: pokemons.length,
+    data: { pokemons: pokemons },
+  });
 }
 
-function responsePokemonCreated (res, pokemon) {
-    response(res, 201, 'success', { data: { pokemon: pokemon} })
+function responsePokemonCreated(res, pokemon) {
+  response(res, 201, 'success', { data: { pokemon: pokemon } });
 }
 
 function responsePokemonDeleted(res, pokemon) {
-    response(res, 204, 'success', { data: { pokemon: pokemon} })
+  response(res, 204, 'success', { data: { pokemon: pokemon } });
 }
 
 exports.getAllPokemons = (req, res) => {
-    const name = req.query.name;
-    let query;
-    if(name) {
-        query = Pokemon.find({ name });
-    } else {
-        query = Pokemon.find({});
+  const name = req.query.name;
+  let query;
+  if (name) {
+    query = Pokemon.find({ name });
+  } else {
+    query = Pokemon.find({});
+  }
+  query.exec((err, pokemons) => {
+    if (err) {
+      responseFail(res, 'Bad request');
     }
-    query.exec((err, pokemons) => {
-        if (err) {
-            responseFail(res, 'Bad request');
-        }
-        responsePokemons(res, pokemons);
-    })
-}
+    responsePokemons(res, pokemons);
+  });
+};
 
 exports.getPokemonById = (req, res) => {
-    const id = req.params.id;
-    const query = Pokemon.findById(id);
-    query.exec((err, pokemon) => {
-        if (err) {
-            responseFail(res, 'Bad request');
-        } else if (!pokemon) {
-            responseBad(res, 'invalid ID')
-        } responsePokemon(res, pokemon);
-    })
+  const id = req.params.id;
+  const query = Pokemon.findById(id);
+  query.exec((err, pokemon) => {
+    if (err) {
+      responseFail(res, 'Bad request');
+    } else if (!pokemon) {
+      responseBad(res, 'invalid ID');
+    }
+    responsePokemon(res, pokemon);
+  });
 };
 
 exports.createNewPokemon = (req, res) => {
-    const newPokemon = {
-        name: '',
-        damage: '',
-        isCaught: '',
-        createdAt: '',
-        ...req.body
-    };
-    Pokemon.create(newPokemon, (err, pokemon) => {
-        if (err) {
-            responseFail(res, 'Bad request');
-        } responsePokemonCreated(res, pokemon);
-    })
+  const newPokemon = {
+    name: '',
+    damage: '',
+    isCaught: '',
+    createdAt: '',
+    ...req.body,
+  };
+  Pokemon.create(newPokemon, (err, pokemon) => {
+    if (err) {
+      responseFail(res, 'Bad request');
+    }
+    responsePokemonCreated(res, pokemon);
+  });
 };
 
 exports.updatePokemon = (req, res) => {
-    const id = req.params.id;
-    const newPokemon = {
-        ...req.body
-    };
-    const query = Pokemon.findByIdAndUpdate(id, newPokemon, {new: true});
-    query.exec((err, pokemon) => {
-        if (err) {
-            return responseFail(res, 'Bad request');
-        } else if (!pokemon) {
-            return responseBad(res, 'invalid ID')
-        }
-        responsePokemon(res, pokemon);
-    })
+  const id = req.params.id;
+  const newPokemon = {
+    ...req.body,
+  };
+  const query = Pokemon.findByIdAndUpdate(id, newPokemon, { new: true });
+  query.exec((err, pokemon) => {
+    if (err) {
+      return responseFail(res, 'Bad request');
+    } else if (!pokemon) {
+      return responseBad(res, 'invalid ID');
+    }
+    responsePokemon(res, pokemon);
+  });
 };
 
 exports.deletePokemon = (req, res) => {
-   const id = req.params.id;
-   const query = Pokemon.findByIdAndDelete(id);
-   query.exec((err, pokemon) => {
-       if (err) {
-           return responseFail(res, 'bad request')
-       } if (!pokemon) {
-           return responseFail(res, 'invalid id')
-       }
-       responsePokemonDeleted(res, null);
-   })
+  const id = req.params.id;
+  const query = Pokemon.findByIdAndDelete(id);
+  query.exec((err, pokemon) => {
+    if (err) {
+      return responseFail(res, 'bad request');
+    }
+    if (!pokemon) {
+      return responseFail(res, 'invalid id');
+    }
+    responsePokemonDeleted(res, null);
+  });
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,22 @@
 const express = require('express');
-const pokemonsController = require('../controllers/pokemonsController')
+const pokemonsController = require('../controllers/pokemonsController');
 
 const router = express.Router();
 
-router.param('id', (req,res, next, val) => {
-    console.log(`Pokemon id is: ${val}`);
-    next();
-})
+router.param('id', (req, res, next, val) => {
+  console.log(`Pokemon id is: ${val}`);
+  next();
+});
 
 router
-    .route('/')
-    .get(pokemonsController.getAllPokemons)
-    .post(pokemonsController.createNewPokemon)
-
+  .route('/')
+  .get(pokemonsController.getAllPokemons)
+  .post(pokemonsController.createNewPokemon);
 
 router
-    .route('/:id')
-    .get(pokemonsController.getPokemonById)
-    .patch(pokemonsController.updatePokemon)
-    .delete(pokemonsController.deletePokemon)
+  .route('/:id')
+  .get(pokemonsController.getPokemonById)
+  .patch(pokemonsController.updatePokemon)
+  .delete(pokemonsController.deletePokemon);
 
 module.exports = router;
